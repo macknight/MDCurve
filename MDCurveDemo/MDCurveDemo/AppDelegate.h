6b66851c326d3eab26e8b8565ddf613f1b72c243
杨晨 <yangchenlarkin@gmail.com>
2013-12-26 23:47:16 +0800

    init project

---
+++ MDCurveDemo/MDCurveDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MDCurveDemo
+//
+//  Created by 杨晨 on 12/26/13.
+//  Copyright (c) 2013 杨晨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MDCurveDemo/MDCurveDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MDCurveDemo
+//
+//  Created by 杨晨 on 12/26/13.
+//  Copyright (c) 2013 杨晨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
